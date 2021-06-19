947077e235da8f50f7e06c163435539c0ac05bbc
yj-matmul <yyj3053@gmail.com>
2021-06-19 17:03:41 +0900

    [20210619] update to learn pointer

--- ch9_pointer/main.go
+++ ch9_pointer/main.go
@@ -9,10 +9,18 @@ func main() {
 	log.Println("myString is set to", myString)
 	changeUsingPointer(&myString)
 	log.Println("after func call myString is set to", myString)
+
+	x := 1.5
+	square(&x)
+	log.Println("x value is", x)
 }
 
 func changeUsingPointer(s *string) {
 	log.Println("s is set to", s)
 	newValue := "Red"
 	*s = newValue
+}
+
+func square(x *float64) {
+	*x = *x * *x
 }
