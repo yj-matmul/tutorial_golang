fd14b280edef56d60d44153262a8482204e54017
yj-matmul <yyj3053@gmail.com>
2021-06-20 16:25:21 +0900

    [20210620] learn channels

---
+++ ch17_channels/go.mod
@@ -0,0 +1,3 @@
+module github.com/yj-matmul/tutorial_go
+
+go 1.16
---
+++ ch17_channels/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/yj-matmul/tutorial_go/helpers"
+)
+
+const numPool = 10
+
+func CalculateValue(intChan chan int) {
+	randomNumber := helpers.RandomNumber(numPool)
+	intChan <- randomNumber
+}
+
+func main() {
+	intChan := make(chan int)
+	defer close(intChan)
+
+	go CalculateValue(intChan)
+
+	num := <-intChan
+	log.Println(num)
+}
