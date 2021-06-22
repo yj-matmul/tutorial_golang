7821f052372731610573eb2e1b775dfe9f8526e0
yj-matmul <yyj3053@gmail.com>
2021-06-23 00:23:54 +0900

    [20210623] learn serving HTML templates with go

---
+++ ch24_serving_HTML_templates/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// Home is the home page handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.html")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "aoubt.page.html")
+}
+
+// renderTemplate is
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	pasredTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := pasredTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template:", err)
+		return
+	}
+}
+
+// main is the main application function
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Println(fmt.Sprintf("Starting application on prot %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
+}
