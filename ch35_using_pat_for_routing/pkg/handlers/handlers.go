bb80b827c4257329f27a523d91e04f8d8571421a
yj-matmul <yyj3053@gmail.com>
2021-06-28 00:27:18 +0900

    [20210628] using pat for routing

---
+++ ch35_using_pat_for_routing/pkg/handlers/handlers.go
@@ -0,0 +1,46 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/tutorial/pkg/config"
+	"github.com/tutorial/pkg/models"
+	"github.com/tutorial/pkg/render"
+)
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+}
+
+// About is the about page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again."
+
+	// send the data to the templates
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
